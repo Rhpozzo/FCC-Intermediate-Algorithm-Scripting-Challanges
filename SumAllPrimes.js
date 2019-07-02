aefba35883f0df8160e83d90d42fffcee9746412
rhpozzo@gmail.com <rhpozzo@gmail.com>
2019-07-02 01:40:27 -0400

    update

--- SumAllPrimes.js
+++ SumAllPrimes.js
@@ -1,19 +1,22 @@
 function sumPrimes(num) {
-    let ctr = [];
-    let prime = [];
-    let sum = 0
+    let ctr = []; //counter;
+    let prime = [];//prime numbers array;
+    let sum = 0// result;
     for (let i = 2; i <= num; i++) {
         if (!ctr[i]) {
             prime.push(i);
+            prime
             for (let j = i < 1; j <= num; j += i) {
                 ctr[j] = true;
             }
         }
     }
+    prime
     for (let i = 0; i < prime.length; i++) {
         sum += prime[i]
     }
     return sum;
 }
 
-console.log(sumPrimes(977));
+console.log(sumPrimes(10));
+
